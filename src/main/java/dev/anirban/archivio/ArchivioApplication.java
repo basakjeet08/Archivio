8515a4c470695b131813794f39863580341ad7c4
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-04 10:22:42 +0530

    initial commit

---
+++ src/main/java/dev/anirban/archivio/ArchivioApplication.java
@@ -0,0 +1,13 @@
+package dev.anirban.archivio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioApplication.class, args);
+	}
+
+}
