a4d42c4bbaaf6f41c020ef535e6515a30873dc13
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-04 11:09:09 +0530

    feat - configured Database connection and Security profile

--- src/main/java/dev/anirban/archivio/ArchivioApplication.java
+++ src/main/java/dev/anirban/archivio/ArchivioApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArchivioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ArchivioApplication.class, args);
 	}
-
 }
